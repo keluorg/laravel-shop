3685b9951aa37db4e2b799a0d87836bd30e21a75
血衫非弧 <admin@kelu.org>
2019-01-27 21:25:56 +0800

    [new] 收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@
 
 Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 Auth::routes(['verify' => true]);
+
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
