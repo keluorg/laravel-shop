09b0d3142dd7b3a72ffcfca9b75c9ee9ae49c128
血衫非弧 <admin@kelu.org>
2019-01-27 20:59:33 +0800

    [new] 邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,5 @@
 //    return view('welcome');
 //});
 
-Route::get('/', 'PagesController@root')->name('root');
-
-Auth::routes();
+Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+Auth::routes(['verify' => true]);
