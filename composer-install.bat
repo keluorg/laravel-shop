9827b2913b0b1b07f5f7f927174a73cb67e373f3
血衫非弧 <admin@kelu.org>
2019-01-27 23:14:12 +0800

    [new] 新增收货地址

--- composer-install.bat
+++ composer-install.bat
@@ -1,2 +1,3 @@
 @echo off
-docker run -it --rm -v d:/GitHub/kelu.org/laravel-shop:/app composer:1.4.2 install --ignore-platform-reqs
+docker run -it --rm -v d:/GitHub/kelu.org/laravel-shop:/app composer:1.4.2 require overtrue/laravel-lang --ignore-platform-reqs
+::docker run -it --rm -v d:/GitHub/kelu.org/laravel-shop:/app composer:1.4.2 install --ignore-platform-reqs
