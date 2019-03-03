2b121c037fc79986d99ba8e004dbf4adbe1d386d
血衫非弧 <admin@kelu.org>
2019-03-03 23:17:35 +0800

    [new] laravel-admin

--- composer-install.bat
+++ composer-install.bat
@@ -1,4 +1,5 @@
 @echo off
-docker run -it --rm -v d:/GitHub/kelu.org/laravel-shop:/app composer:1.4.2 require encore/laravel-admin "1.6.8" --ignore-platform-reqs
+docker run -it --rm -v d:/GitHub/kelu.org/laravel-shop:/app composer:1.4.2 install --ignore-platform-reqs
+::docker run -it --rm -v d:/GitHub/kelu.org/laravel-shop:/app composer:1.4.2 require encore/laravel-admin "1.6.8" --ignore-platform-reqs
 ::docker run -it --rm -v d:/GitHub/kelu.org/laravel-shop:/app composer:1.4.2 require overtrue/laravel-lang --ignore-platform-reqs
 ::docker run -it --rm -v d:/GitHub/kelu.org/laravel-shop:/app composer:1.4.2 install --ignore-platform-reqs
