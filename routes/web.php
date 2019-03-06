b0fa92a1da71b8f5415e0ba7f050bc959019ed8c
血衫非弧 <admin@kelu.org>
2019-03-06 23:28:22 +0800

    [new] 用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@
 //    return view('welcome');
 //});
 
-Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+//Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 Auth::routes(['verify' => true]);
 
 Route::group(['middleware' => ['auth', 'verified']], function() {
@@ -25,5 +27,6 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+
 
 });
