53f55b5462aa7f37d44c9a36bb3bc267243d310b
血衫非弧 <admin@kelu.org>
2019-03-23 20:52:40 +0800

    [new] 订单模型

--- database/migrations/2019_03_17_153417_create_orders_table.php
+++ database/migrations/2019_03_17_153417_create_orders_table.php
@@ -15,6 +15,22 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('no')->unique();
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->text('address');
+            $table->decimal('total_amount', 10, 2);
+            $table->text('remark')->nullable();
+            $table->dateTime('paid_at')->nullable();
+            $table->string('payment_method')->nullable();
+            $table->string('payment_no')->nullable();
+            $table->string('refund_no')->unique()->nullable();
+            $table->boolean('closed')->default(false);
+            $table->boolean('reviewed')->default(false);
+            $table->string('refund_status')->default(\App\Models\Order::REFUND_STATUS_PENDING);
+            $table->string('ship_status')->default(\App\Models\Order::SHIP_STATUS_PENDING);
+            $table->text('ship_data')->nullable();
+            $table->text('extra')->nullable();
             $table->timestamps();
         });
     }
